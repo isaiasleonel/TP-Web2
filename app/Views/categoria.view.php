2bd0b5bb4303f90293f1df49c01691336b4e61ef
Isaias Leonel Sardina <isaias.sardina@gmail.com>
2022-11-30 19:50:47 -0300

    Retoques Visuales

--- app/Views/categoria.view.php
+++ app/Views/categoria.view.php
@@ -10,19 +10,16 @@ class categoriaView
         $this->smarty = new Smarty();
     }
 
-
+    // Para Agregar Categorias
     function  formCategoria($categorias)
     {
-
         $this->smarty->assign('categorias', $categorias);
-
         $this->smarty->display('./Templates/addFormcategoria.tpl');
     }
 
-
+    // Para Editar como ADM
     function  viewEditar($categoriaID, $select)
     {
-
         $this->smarty->assign('categoriaID', $categoriaID);
         $this->smarty->assign('select', $select);
         $this->smarty->display('./Templates/admHome.tpl');
